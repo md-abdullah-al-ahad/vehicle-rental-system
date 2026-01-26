8d439366844b0961b147a68b80f43a4d67073241
Md. Abdullah Al Ahad <abdullah.ruet22@gmail.com>
2026-01-27 03:08:55 +0600

    fix: prevent users from seeding role as admin

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -3,11 +3,11 @@ import { pool } from "../../config/db";
 import { config } from "dotenv";
 import jwt from "jsonwebtoken";
 const createUser = async (payload: Record<string, unknown>) => {
-  const { name, email, password, phone, role } = payload;
+  const { name, email, password, phone } = payload;
   const hashedPassword = await bcrypt.hash(password as string, 10);
   const result = await pool.query(
     "INSERT INTO users (name, email, password, phone, role) VALUES ($1, $2, $3, $4, $5) RETURNING id, name, email, phone, role",
-    [name, email, hashedPassword, phone, role]
+    [name, email, hashedPassword, phone, "customer"],
   );
   return result.rows[0];
 };
@@ -37,7 +37,7 @@ const loginUser = async (email: string, password: string) => {
     secret,
     {
       expiresIn: "7d",
-    }
+    },
   );
   return { token, user };
 };
